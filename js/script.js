c96f5a57e8173f974a331798813dea6916cb6eb8
FaeelViegas <victorviegas036@gmail.com>
2024-08-02 18:11:33 -0300

    acentuação nas palavras secretas/ alterações no estilo

--- js/script.js
+++ js/script.js
@@ -3,88 +3,96 @@ let secretWordDrawn;
 let attempt = 6;
 let list = [];
 const words = [
+    { name: "ABACAXI", category: "FRUTAS" },
+    { name: "BANANA", category: "FRUTAS" },
+    { name: "CEREJA", category: "FRUTAS" },
+    { name: "DAMASCO", category: "FRUTAS" },
+    { name: "ESCARAMUJO", category: "FRUTAS" },
+    { name: "FIGO", category: "FRUTAS" },
+    { name: "GOIABA", category: "FRUTAS" },
+    { name: "HIBISCO", category: "FRUTAS" },
+    { name: "JACA", category: "FRUTAS" },
+    { name: "KIWI", category: "FRUTAS" },
+    { name: "LIMÃO", category: "FRUTAS" },
+    { name: "MANGA", category: "FRUTAS" },
+    { name: "NECTARINA", category: "FRUTAS" },
+    { name: "PITANGA", category: "FRUTAS" },
+    { name: "QUINCE", category: "FRUTAS" },
+    { name: "ROMÃ", category: "FRUTAS" },
+    { name: "SAPOTI", category: "FRUTAS" },
+    { name: "TAMARINDO", category: "FRUTAS" },
+    { name: "UMBU", category: "FRUTAS" },
+    { name: "UVAS", category: "FRUTAS" },
     { name: "BRASIL", category: "PAISES" },
     { name: "ARGENTINA", category: "PAISES" },
-    { name: "ALEMANHA", category: "PAISES" },
-    { name: "FRANÇA", category: "PAISES" },
-    { name: "JAPÃO", category: "PAISES" },
+    { name: "CHILE", category: "PAISES" },
+    { name: "COLÔMBIA", category: "PAISES" },
+    { name: "PERU", category: "PAISES" },
+    { name: "VENEZUELA", category: "PAISES" },
+    { name: "PARAGUAI", category: "PAISES" },
+    { name: "URUGUAI", category: "PAISES" },
+    { name: "BOLÍVIA", category: "PAISES" },
+    { name: "EQUADOR", category: "PAISES" },
     { name: "CANADÁ", category: "PAISES" },
-    { name: "AUSTRÁLIA", category: "PAISES" },
-    { name: "CHINA", category: "PAISES" },
-    { name: "ITÁLIA", category: "PAISES" },
-    { name: "PIZZA", category: "COMIDA" },
-    { name: "HAMBURGER", category: "COMIDA" },
-    { name: "LASANHA", category: "COMIDA" },
-    { name: "TACO", category: "COMIDA" },
-    { name: "SUSHI", category: "COMIDA" },
-    { name: "PASTEL", category: "COMIDA" },
-    { name: "COXINHA", category: "COMIDA" },
-    { name: "BRIGADEIRO", category: "COMIDA" },
-    { name: "FEIJOADA", category: "COMIDA" },
-    { name: "CHURRASCO", category: "COMIDA" },
-    { name: "CACHORRO", category: "ANIMAL" },
-    { name: "GATO", category: "ANIMAL" },
-    { name: "ELEFANTE", category: "ANIMAL" },
-    { name: "GIRAFA", category: "ANIMAL" },
-    { name: "LEÃO", category: "ANIMAL" },
-    { name: "TIGRE", category: "ANIMAL" },
-    { name: "PÁSSARO", category: "ANIMAL" },
-    { name: "PEIXE", category: "ANIMAL" },
-    { name: "RATO", category: "ANIMAL" },
-    { name: "COELHO", category: "ANIMAL" },
-    { name: "TITANIC", category: "FILME" },
-    { name: "AVATAR", category: "FILME" },
-    { name: "INCEPTION", category: "FILME" },
-    { name: "INTERSTELLAR", category: "FILME" },
-    { name: "GLADIADOR", category: "FILME" },
-    { name: "MATRIX", category: "FILME" },
-    { name: "CORINGA", category: "FILME" },
-    { name: "BATMAN", category: "FILME" },
-    { name: "VINGADORES", category: "FILME" },
-    { name: "SUPERMAN", category: "FILME" },
-    { name: "TUBARÃO", category: "FILME" },
-    { name: "ESPANHA", category: "PAISES" },
-    { name: "PORTUGAL", category: "PAISES" },
-    { name: "RÚSSIA", category: "PAISES" },
-    { name: "ÍNDIA", category: "PAISES" },
     { name: "MÉXICO", category: "PAISES" },
-    { name: "EGITO", category: "PAISES" },
-    { name: "SUÍÇA", category: "PAISES" },
-    { name: "MACARRÃO", category: "COMIDA" },
-    { name: "SORVETE", category: "COMIDA" },
-    { name: "BOLO", category: "COMIDA" },
-    { name: "SALADA", category: "COMIDA" },
-    { name: "CHOCOLATE", category: "COMIDA" },
-    { name: "PÃO", category: "COMIDA" },
-    { name: "BISCOITO", category: "COMIDA" },
-    { name: "TORTA", category: "COMIDA" },
-    { name: "FRUTAS", category: "COMIDA" },
-    { name: "CAVALO", category: "ANIMAL" },
-    { name: "VACA", category: "ANIMAL" },
-    { name: "OVELHA", category: "ANIMAL" },
-    { name: "CABRA", category: "ANIMAL" },
-    { name: "PANDA", category: "ANIMAL" },
-    { name: "LOBO", category: "ANIMAL" },
-    { name: "RAPOSA", category: "ANIMAL" },
-    { name: "URSO", category: "ANIMAL" },
-    { name: "MACACO", category: "ANIMAL" },
-    { name: "CROCODILO", category: "ANIMAL" },
-    { name: "THOR", category: "FILME" },
-    { name: "MAÇÃ", category: "FRUTA" },
-    { name: "BANANA", category: "FRUTA" },
-    { name: "LARANJA", category: "FRUTA" },
-    { name: "MORANGO", category: "FRUTA" },
-    { name: "ABACAXI", category: "FRUTA" },
-    { name: "MANGA", category: "FRUTA" },
-    { name: "UVA", category: "FRUTA" },
-    { name: "PÊSSEGO", category: "FRUTA" },
-    { name: "MELANCIA", category: "FRUTA" },
-    { name: "KIWI", category: "FRUTA" },
-    { name: "PERA", category: "FRUTA" },
-    { name: "CEREJA", category: "FRUTA" },
-    { name: "FRAMBOESA", category: "FRUTA" },
-    { name: "MELÃO", category: "FRUTA" },
-    { name: "COCO", category: "FRUTA" }
+    { name: "CUBA", category: "PAISES" },
+    { name: "FRANÇA", category: "PAISES" },
+    { name: "ITÁLIA", category: "PAISES" },
+    { name: "ESPANHA", category: "PAISES" },
+    { name: "ALEMANHA", category: "PAISES" },
+    { name: "PORTUGAL", category: "PAISES" },
+    { name: "JAPÃO", category: "PAISES" },
+    { name: "CHINA", category: "PAISES" },
+    { name: "LEÃO", category: "ANIMAIS" },
+    { name: "ELEFANTE", category: "ANIMAIS" },
+    { name: "GIRAFA", category: "ANIMAIS" },
+    { name: "TIGRE", category: "ANIMAIS" },
+    { name: "URSO", category: "ANIMAIS" },
+    { name: "COALA", category: "ANIMAIS" },
+    { name: "GORILA", category: "ANIMAIS" },
+    { name: "CAVALO", category: "ANIMAIS" },
+    { name: "CACHORRO", category: "ANIMAIS" },
+    { name: "GATO", category: "ANIMAIS" },
+    { name: "CROCODILO", category: "ANIMAIS" },
+    { name: "CANGURU", category: "ANIMAIS" },
+    { name: "PINGUIM", category: "ANIMAIS" },
+    { name: "ZEBRA", category: "ANIMAIS" },
+    { name: "HIPOPÓTAMO", category: "ANIMAIS" },
+    { name: "BALEIA", category: "ANIMAIS" },
+    { name: "GOLFINHO", category: "ANIMAIS" },
+    { name: "PANDA", category: "ANIMAIS" },
+    { name: "RINOCERONTE", category: "ANIMAIS" },
+    { name: "TARTARUGA", category: "ANIMAIS" },
+    { name: "FRIENDS", category: "TV e CINEMA" },
+    { name: "BREAKING BAD", category: "TV e CINEMA" },
+    { name: "GAME OF THRONES", category: "TV e CINEMA" },
+    { name: "STRANGER THINGS", category: "TV e CINEMA" },
+    { name: "NARCOS", category: "TV e CINEMA" },
+    { name: "THE WITCHER", category: "TV e CINEMA" },
+    { name: "THE OFFICE", category: "TV e CINEMA" },
+    { name: "LOST", category: "TV e CINEMA" },
+    { name: "BLACK MIRROR", category: "TV e CINEMA" },
+    { name: "VIKINGS", category: "TV e CINEMA" },
+    { name: "THE BOYS", category: "TV e CINEMA" },
+    { name: "LUCIFER", category: "TV e CINEMA" },
+    { name: "SHERLOCK", category: "TV e CINEMA" },
+    { name: "SUPERNATURAL", category: "TV e CINEMA" },
+    { name: "O SENHOR DOS ANÉIS", category: "TV e CINEMA" },
+    { name: "STAR WARS", category: "TV e CINEMA" },
+    { name: "HARRY POTTER", category: "TV e CINEMA" },
+    { name: "TITANIC", category: "TV e CINEMA" },
+    { name: "O REI LEÃO", category: "TV e CINEMA" },
+    { name: "JURASSIC PARK", category: "TV e CINEMA" },
+    { name: "O EXTERMINADOR DO FUTURO", category: "TV e CINEMA" },
+    { name: "CLUBE DA LUTA", category: "TV e CINEMA" },
+    { name: "GLADIADOR", category: "TV e CINEMA" },
+    { name: "MATRIX", category: "TV e CINEMA" },
+    { name: "O ILUMINADO", category: "TV e CINEMA" },
+    { name: "BLADE RUNNER", category: "TV e CINEMA" },
+    { name: "CORINGA", category: "TV e CINEMA" },
+    { name: "OS VINGADORES", category: "TV e CINEMA" },
+    { name: "A BELA E A FERA", category: "TV e CINEMA" },
+    { name: "PANICO", category: "TV e CINEMA" }
 ];
 
 createSecretWord();
@@ -146,7 +154,10 @@ function styleLetter(key, con) {
 
 function compareList(letter) {
     let win = true;
-    const position = secretWordDrawn.indexOf(letter);
+    const normalizedSecretWord = secretWordDrawn.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    const normalizedLetter = letter.normalize('NFD').replace(/[\u0300-\u036f']/g, "");
+
+    const position = normalizedSecretWord.indexOf(normalizedLetter);
     if (position < 0) {
         attempt--;
         loadImage();
@@ -155,19 +166,20 @@ function compareList(letter) {
         }
     } else {
         styleLetter("key-" + letter, true);
-        for (i = 0; i < secretWordDrawn.length; i++) {
-            if (secretWordDrawn[i] == letter) {
-                list[i] = letter;
+        for (let i = 0; i < secretWordDrawn.length; i++) {
+            if (normalizedSecretWord[i] === normalizedLetter) {
+                list[i] = secretWordDrawn[i];
             }
         }
     }
-    for (i = 0; i < secretWordDrawn.length; i++) {
-        if (secretWordDrawn[i] != list[i]) {
+
+    for (let i = 0; i < secretWordDrawn.length; i++) {
+        if (secretWordDrawn[i] !== list[i]) {
             win = false;
         }
     }
 
-    if (win == true) {
+    if (win) {
         openModal("PARABÉNS!", "Você acertou...🤗");
         attempt = 0;
     }
